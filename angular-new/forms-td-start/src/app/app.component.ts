a747d62bd3f117103345d9012661e725faaf257e
Shravya Narayan Sarma <shravya.sarma@gmail.com>
2023-07-21 18:47:22 -0400

    Add new files

---
+++ angular-new/forms-td-start/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  defaultQuestion = 'pet';
+  answer : String = '';
+  genders:['male','female'];
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form:NgForm){
+    console.log("onSubmit clicked!!!");
+    console.log(form);
+  }
+}
